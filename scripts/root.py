70cb7579fb02581d965582087c03e87ad898064d
bilbo <bilbo.derbyshire@gmail.com>
2023-06-09 23:11:07 +0100

    Created title bar for every scene

--- scripts/root.py
+++ scripts/root.py
@@ -5,9 +5,10 @@ import os
 
 class ReportWriter(ctk.CTk):
     def __init__(self):
-        super().__init__()
+        super().__init__(fg_color=ROOT_BG)
 
         ctk.set_default_color_theme(os.path.join(os.getcwd(), "scripts/app-theme.json"))
+        ctk.set_appearance_mode("dark")
 
         ws = self.winfo_screenwidth()
         hs = self.winfo_screenheight()
@@ -19,13 +20,19 @@ class ReportWriter(ctk.CTk):
         self.title(WINDOW_TITLE)
         self.iconbitmap(default=os.path.join(os.getcwd(), "images/app-logo.ico"))
 
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
         self.frames = {}
+        self.__setup_frames()
+        # self.show_frame()
 
     def __setup_frames(self):
         current_frame_list = []
         for frame in current_frame_list:
-            new_frame = frame()
+            new_frame = frame(self)
             self.frames[frame] = new_frame
+            new_frame.grid(row=0, column=0, sticky="nsew")
 
     def show_frame(self, frame_to_show):
         frame = self.frames[frame_to_show]
--- scripts/settings.py
+++ scripts/settings.py
@@ -4,3 +4,20 @@ WIDTH = 1000
 HEIGHT = 600
 GEOMETRY = f"{WIDTH}x{HEIGHT}"
 WINDOW_TITLE = "School Report Writer"
+DEFAULT_PAD = 10
+DEFAULT_PAD_COMPLETE = {
+    "padx": DEFAULT_PAD,
+    "pady": DEFAULT_PAD
+}
+
+
+# Fonts
+
+TITLE_FONT = {"family": "Segoe UI",
+              "size": 30,
+              "weight": "bold"}
+
+
+# Colours
+
+ROOT_BG = ("#F2F2F2", "#23272D")
