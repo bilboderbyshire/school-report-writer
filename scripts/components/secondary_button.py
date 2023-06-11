664294ca63ef01fe0fd242f7766355c1365c38fd
bilbo <bilbo.derbyshire@gmail.com>
2023-06-11 22:15:07 +0100

    Created secondary button component

---
+++ scripts/components/secondary_button.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+from ..settings import *
+
+
+class SecondaryButton(ctk.CTkButton):
+    def __init__(self, master,
+                 fg_color=SECONDARY_BUTTON_FG_COLOR,
+                 hover_color=SECONDARY_BUTTON_HOVER_COLOR,
+                 text_color=SECONDARY_BUTTON_TEXT_COLOR,
+                 **kwargs):
+        super().__init__(master,
+                         fg_color=fg_color,
+                         hover_color=hover_color,
+                         text_color=text_color,
+                         **kwargs)
