3023ca4bec97ee065942218f829786f5d696e00a
bilbo <bilbo.derbyshire@gmail.com>
2023-06-11 14:05:03 +0100

    Created Login window login and cancel buttons

--- scripts/login_window/login_frame.py
+++ scripts/login_window/login_frame.py
@@ -34,8 +34,21 @@ class LoginFrame(ctk.CTkFrame):
         self.register_button = SmallLabelButton(self,
                                                 text="Register account",
                                                 width=5)
-        self.register_button.grid(row=5, column=2, sticky="e", pady=(0, DEFAULT_PAD), padx=DEFAULT_PAD)
+        self.register_button.grid(row=5, column=1, columnspan=2, sticky="e", pady=(0, DEFAULT_PAD), padx=DEFAULT_PAD)
 
-        self.rowconfigure([0, 1, 2, 3, 4, 5], weight=0)
-        self.columnconfigure([0, 1, 2], weight=1)
+        self.login_button = ctk.CTkButton(self,
+                                          text="Login",
+                                          font=ctk.CTkFont(**NORMAL_LABEL_FONT),
+                                          border_spacing=5)
+        self.login_button.grid(row=7, column=1, sticky="ew", padx=(0, DEFAULT_PAD))
+
+        self.cancel_button = ctk.CTkButton(self,
+                                           text="Cancel",
+                                           font=ctk.CTkFont(**NORMAL_LABEL_FONT),
+                                           border_spacing=5)
+        self.cancel_button.grid(row=7, column=2, sticky="ew", padx=(0, DEFAULT_PAD))
+
+        self.rowconfigure(0, weight=0)
+        self.rowconfigure([1, 2, 3, 4, 5, 6, 7], weight=1, uniform="rows")
+        self.columnconfigure([0, 1, 2], weight=1, uniform="columns")
 
