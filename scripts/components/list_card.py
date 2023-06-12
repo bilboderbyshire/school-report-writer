2f1c5f0cad039f538d62149148a8308bc86d70ca
bilbo <bilbo.derbyshire@gmail.com>
2023-06-12 19:42:57 +0100

    Modified list card component to have hover and click effects

--- scripts/components/list_card.py
+++ scripts/components/list_card.py
@@ -5,7 +5,25 @@ from ..containers import SingleReportSet
 
 class ListCard(ctk.CTkFrame):
     def __init__(self, master, report_set: SingleReportSet,
-                 fg_color=ROOT_BG, text_color=STANDARD_TEXT_COLOR):
+                 fg_color=LABEL_CARD_COLOR, text_color=STANDARD_TEXT_COLOR,
+                 hover_color=LABEL_CARD_HOVER_COLOR,
+                 command=None):
         super().__init__(master,
                          height=80,
                          fg_color=fg_color)
+
+        self.command = command
+        self.hover_color = hover_color
+        self.main_color = fg_color
+
+        self.bind("<Enter>", lambda event: self.on_hover())
+        self.bind("<Leave>", lambda event: self.on_mouse_leave())
+
+        if self.command is not None:
+            self.bind("<Button-1>", lambda event: self.command())
+
+    def on_hover(self):
+        self.configure(fg_color=self.hover_color)
+
+    def on_mouse_leave(self):
+        self.configure(fg_color=self.main_color)
--- scripts/main_menu/reports_frame.py
+++ scripts/main_menu/reports_frame.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 from ..settings import *
 from ..database import RUNNING_DB
 from ..containers import SingleReportSet
-from ..components import AutohidingScrollableAndLoadingFrame
+from ..components import AutohidingScrollableAndLoadingFrame, ListCard
 
 
 class ReportsScrollableFrame(AutohidingScrollableAndLoadingFrame):
@@ -13,7 +13,19 @@ class ReportsScrollableFrame(AutohidingScrollableAndLoadingFrame):
                          label_fg_color="transparent",
                          label_text="Reports")
 
+    def build_frame(self):
+        self.loading_label.destroy()
+        self.update_idletasks()
 
+        for i in range(6):
+            new_card = ListCard(self, None, command=self.card_command)
+            new_card.grid(row=i, column=0, sticky="ew", padx=DEFAULT_PAD-7)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=0)
+
+    def card_command(self):
+        print("Card clicked")
 
 
 
