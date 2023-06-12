c0bb68c0c46b58e42ab1ab381434b0360ca74949
bilbo <bilbo.derbyshire@gmail.com>
2023-06-12 22:41:44 +0100

    Created Report set card component

--- scripts/settings.py
+++ scripts/settings.py
@@ -19,12 +19,22 @@ LOGIN_WIDTH = 400
 LOGIN_HEIGHT = 500
 LOGIN_GEOMETRY = f"{LOGIN_WIDTH}x{LOGIN_HEIGHT}"
 
+# Frame settings
+SMALL_PAD = 5
+SMALL_PAD_COMPLETE = {
+    "padx": SMALL_PAD,
+    "pady": SMALL_PAD
+}
 
 # Fonts
 
 TITLE_FONT = {"family": "Segoe UI",
               "size": 30,
               "weight": "bold"}
+
+SECONDARY_TITLE_FONT = {"family": "Segoe UI",
+                        "size": 23,
+                        "weight": "bold"}
 
 ENTRY_FONT = {"family": "Segoe UI",
               "size": 17}
@@ -49,4 +59,9 @@ SECONDARY_BUTTON_TEXT_COLOR = ("#242424", "#DCE4EE")
 STANDARD_SCROLLBAR_BUTTON_COLOR = ("gray55", "gray41")
 STANDARD_SCROLLBAR_BUTTON_HOVER_COLOR = ("gray40", "gray53")
 LABEL_CARD_COLOR = ("#F9F9F9", "#2F333A")
-LABEL_CARD_HOVER_COLOR = ("#B8B8B8", "#282D34")
+LABEL_CARD_HOVER_COLOR = ("#C9C9C9", "#282D34")
+
+NOT_COMPLETED_COLOR = (166, 61, 58)
+ALMOST_COMPLETED_COLOR = (166, 158, 46)
+COMPLETED_COLOR = (69, 161, 22)
+PROGRESS_BAR_BASE_COLOR = ("#DADADA", "#4A4D50")
