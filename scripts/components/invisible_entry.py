83cfbd30dc5be8dacf1c1526f52f7c01f0b41ba6
bilbo <bilbo.derbyshire@gmail.com>
2023-06-13 21:04:11 +0100

    Created invisible entry component

---
+++ scripts/components/invisible_entry.py
@@ -0,0 +1,45 @@
+import customtkinter as ctk
+from ..settings import *
+from PIL import Image
+import os
+
+
+class InvisibleEntry(ctk.CTkFrame):
+    def __init__(self, master,
+                 fg_color="transparent",
+                 text_color=STANDARD_TEXT_COLOR,
+                 placeholder_text="Invisible entry",
+                 **kwargs):
+        super().__init__(master,
+                         fg_color=fg_color)
+
+        self.text_entry = ctk.CTkEntry(
+            self,
+            font=ctk.CTkFont(**SECONDARY_TITLE_FONT),
+            fg_color=fg_color,
+            text_color=text_color,
+            placeholder_text=placeholder_text,
+            placeholder_text_color=text_color,
+            border_width=0,
+            **kwargs)
+
+        self.edit_image = ctk.CTkImage(
+                light_image=Image.open(os.path.join(os.getcwd(), "images/light-pencil.png")),
+                dark_image=Image.open(os.path.join(os.getcwd(), "images/dark-pencil.png")),
+                size=(15, 15)
+            )
+
+        self.image_label = ctk.CTkLabel(
+            self,
+            text="",
+            fg_color=fg_color,
+            image=self.edit_image,
+            anchor="center",
+        )
+
+        self.image_label.grid(row=0, column=0, sticky="nsew", padx=(0, DEFAULT_PAD))
+        self.text_entry.grid(row=0, column=1, sticky="nsew")
+
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=0)
+        self.columnconfigure(1, weight=1)
