4abde1d544d709fc239fceec3bc96429a4030121
bilbo <bilbo.derbyshire@gmail.com>
2023-06-13 17:58:48 +0100

    Modified title bar to have an optional back button
    The idea is this button will allow the user to quickly navigate to the previous scene

--- scripts/title_bar.py
+++ scripts/title_bar.py
@@ -2,7 +2,8 @@ import customtkinter as ctk
 from .settings import *
 from PIL import Image
 import os
-from .components import TitleLabel
+from .components import TitleLabel, SmallLabelButton
+from typing import Callable
 
 
 def change_theme():
@@ -20,15 +21,30 @@ class TitleBar(ctk.CTkFrame):
     """
     The title bar that is present in every frame, the text in the title can be changed by a given method
     """
-    def __init__(self, master, title_text, refresh_command=None):
+    def __init__(self, master,
+                 title_text,
+                 refresh_command=None,
+                 back_command: Callable[..., None] | None = None):
         super().__init__(master,
                          fg_color="transparent")
+
+        self.back_command = back_command
 
         # The label containing the text that explains what the current scene is showing
         self.title_label = TitleLabel(self,
                                       title_text)
 
-        self.title_label.grid(row=0, column=0, sticky="nsew", **DEFAULT_PAD_COMPLETE)
+        self.title_label.grid(row=0, column=0, sticky="nw", pady=(DEFAULT_PAD, 0), padx=DEFAULT_PAD)
+
+        if self.back_command is not None:
+            self.back_button = SmallLabelButton(self,
+                                                text="Back",
+                                                width=0,
+                                                height=0,
+                                                border_spacing=0,
+                                                command=self.back_command)
+            self.back_button.grid(row=1, column=0, sticky="w", padx=DEFAULT_PAD)
+            self.title_label.grid_configure(pady=(0, 0))
 
         # The image within the theme change button
         self.theme_image = ctk.CTkImage(
@@ -49,7 +65,7 @@ class TitleBar(ctk.CTkFrame):
             hover_color=BUTTON_HOVER_COLOR,
             corner_radius=8
         )
-        self.theme_button.grid(row=0, column=2, sticky="e", pady=DEFAULT_PAD)
+        self.theme_button.grid(row=0, rowspan=2, column=2, sticky="e", pady=DEFAULT_PAD)
 
         self.command = refresh_command
 
@@ -72,8 +88,9 @@ class TitleBar(ctk.CTkFrame):
                 corner_radius=8
             )
 
-            self.refresh_button.grid(row=0, column=1, sticky="e", **DEFAULT_PAD_COMPLETE)
+            self.refresh_button.grid(row=0, rowspan=2, column=1, sticky="e", **DEFAULT_PAD_COMPLETE)
 
         self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=0)
         self.columnconfigure(0, weight=1)
         self.columnconfigure([1, 2], weight=0)
