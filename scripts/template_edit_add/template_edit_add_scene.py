972f921a204e3ade2fff11c7d13e1f523be73396
bilbo <bilbo.derbyshire@gmail.com>
2023-06-19 15:45:36 +0100

    Modified template scene to fit around new section collection in database

--- scripts/template_edit_add/template_edit_add_scene.py
+++ scripts/template_edit_add/template_edit_add_scene.py
@@ -5,7 +5,7 @@ from ..components import Separator, InvisibleEntry
 from ..app_engine import AppEngine
 from .section_scrollframe import SectionScrollableFrame
 from .pieces_scrollframe import PiecesScrollableFrame
-from ..containers import ReportTemplate, NewPieceRecord, IndividualPiece
+from ..containers import ReportTemplate, NewPieceRecord, IndividualPiece, TemplateSection, NewSectionRecord
 import CTkMessagebox as ctkmb
 
 
@@ -16,10 +16,10 @@ class TemplateScene(ctk.CTkFrame):
         self.app_engine = app_engine
         self.prev_scene_string = None
         self.working_template: ReportTemplate = None
-        self.structured_pieces: dict[int, dict[str, IndividualPiece]] = None
+        self.structured_pieces: dict[str, dict[str, IndividualPiece]] = None
 
-        self.selected_section: int = None
-        self.selected_piece: str = None
+        self.selected_section: str | None = None
+        self.selected_piece: str | None = None
 
     def __build_frame(self):
 
@@ -128,29 +128,58 @@ class TemplateScene(ctk.CTkFrame):
         else:
             return False
 
-    def new_section_selected(self, section_num: int):
+    def new_section_selected(self, section: TemplateSection):
         if self.selected_section is not None:
             self.section_frame.all_cards[self.selected_section].card_deselected()
-        self.selected_section = section_num
+
+        self.selected_section = section.id
         self.section_frame.all_cards[self.selected_section].card_selected()
         self.pieces_frame.build_pieces_frame(self.selected_section)
         self.new_piece_selected()
         self.pieces_frame.check_scrollbar_needed()
 
     def add_section(self, _):
-        new_section_num = max(self.structured_pieces.keys(), default=0) + 1
+        current_section_titles = []
 
-        self.structured_pieces[new_section_num] = {}
+        for section in self.app_engine.copy_of_section_collection.values():
+            if section.template == self.working_template.id:
+                current_section_titles.append(section.section_title)
+
+        max_title_number = 1
+        for title in current_section_titles:
+            if title[0:11] == "New section":
+                try:
+                    if title[11::] == "" and max_title_number == 1:
+                        max_title_number = 2
+                    elif int(title[11::]) >= max_title_number:
+                        max_title_number = int(title[11::]) + 1
+                except ValueError:
+                    continue
+
+        new_section_name = f"New section {max_title_number}"
+        new_section_id = f"@{self.app_engine.create_new_record_id('template_sections')}"
+
+        new_section = TemplateSection(NewSectionRecord(
+            section_id=new_section_id,
+            section_title=new_section_name,
+            template_id=self.working_template.id
+        ))
+
+        self.structured_pieces[new_section_id] = {}
+        self.app_engine.copy_of_section_collection[new_section_id] = new_section
         self.section_frame.build_section_frame()
-        self.new_section_selected(new_section_num)
+        self.pieces_frame.build_pieces_frame(new_section_id)
+
+        self.new_section_selected(new_section)
+        self.new_piece_selected()
         self.check_if_scroll_needed()
 
-    def delete_section(self, section: int):
-        if self.structured_pieces[section]:
+    def delete_section(self, section: TemplateSection):
+        if self.structured_pieces[section.id]:
             warning_box = ctkmb.CTkMessagebox(
                 title="Warning",
-                message=f"Are you sure you want to delete section {section} and all the pieces inside?\n"
-                        f"You will not be able to undo this move",
+                message=f"Are you sure you want to delete section '{section.section_title}' and all the pieces "
+                        f"inside?\nYou will not be able to undo this move",
                 icon="cancel",
                 option_2="Yes",
                 option_1="No")
@@ -159,13 +188,13 @@ class TemplateScene(ctk.CTkFrame):
             if warning_box.get() == "No":
                 return
 
-        for piece_id, piece in self.structured_pieces[section].items():
+        for piece_id, piece in self.structured_pieces[section.id].items():
             self.app_engine.copy_of_piece_collection.pop(piece_id)
 
-        self.structured_pieces.pop(section)
+        self.structured_pieces.pop(section.id)
         self.section_frame.build_section_frame()
 
-        if section == self.selected_section:
+        if section.id == self.selected_section:
             all_sections = list(self.structured_pieces.keys())
             if all_sections:
                 self.selected_section = all_sections[0]
@@ -182,7 +211,7 @@ class TemplateScene(ctk.CTkFrame):
 
     def add_piece(self, _):
         new_piece_id = f"@{self.app_engine.create_new_record_id('report_pieces')}"
-        new_piece = IndividualPiece(NewPieceRecord(new_piece_id, self.selected_section, self.working_template.id))
+        new_piece = IndividualPiece(NewPieceRecord(new_piece_id, self.selected_section))
 
         self.app_engine.copy_of_piece_collection[new_piece_id] = new_piece
         self.structured_pieces[self.selected_section][new_piece_id] = new_piece
