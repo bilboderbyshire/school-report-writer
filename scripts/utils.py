ab79d7b0ff91e3661d0466964539067992020a88
bilbo <bilbo.derbyshire@gmail.com>
2023-06-21 12:24:46 +0100

    Changed the tag finding method into a utils function
    This allows the function to be used by other text boxes in the program (eg. The report writer)

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,6 @@
+import customtkinter as ctk
+
+
 def rgb_to_hex(rgb: tuple[int, int, int]) -> str:
     r_val = hex(rgb[0])[2::]
     if len(r_val) == 1:
@@ -12,3 +15,59 @@ def rgb_to_hex(rgb: tuple[int, int, int]) -> str:
         b_val = "0" + b_val
 
     return f"#{r_val}{g_val}{b_val}"
+
+
+def find_tags_in_text(text: str, textbox: ctk.CTkTextbox, variable_tracker: dict[str, list[str]]) -> \
+        dict[str, list[str]]:
+    all_tags = textbox.tag_names()
+    current_line = 1
+    current_tag_start_index = -1
+    current_tag_end_index = 0
+    in_tag = False
+    current_tag = ""
+    for char in text:
+        if char == "\n":
+            current_line += 1
+            current_tag_start_index = -1
+            continue
+
+        if textbox.tag_names(f"{current_line}.{current_tag_start_index + 1}"):
+            current_tag_start_index += 1
+        elif in_tag and char == "{":
+            current_tag_end_index += 1
+            current_tag_start_index = current_tag_end_index
+            current_tag = char
+        elif in_tag and char != "}":
+            current_tag_end_index += 1
+            current_tag += char
+        elif char == "{":
+            current_tag_start_index += 1
+            current_tag_end_index = current_tag_start_index
+            current_tag = char
+            in_tag = True
+        elif char == "}":
+            current_tag += char
+            current_tag_end_index += 1
+            in_tag = False
+            if ":" in current_tag:
+                final_tag = current_tag.split(":")[0][1::]
+            else:
+                final_tag = current_tag[1:-1]
+            if final_tag in all_tags:
+                textbox.tag_add(
+                    final_tag,
+                    f"{current_line}.{current_tag_start_index}",
+                    f"{current_line}.{current_tag_end_index + 1}")
+                if final_tag in variable_tracker.keys():
+                    variable_tracker[final_tag].append(current_tag)
+                else:
+                    variable_tracker[final_tag] = [current_tag]
+
+            current_tag = ""
+            current_tag_start_index = current_tag_end_index
+            current_tag_end_index = 0
+        else:
+            current_tag_start_index += 1
+
+    textbox.update()
+    return variable_tracker
